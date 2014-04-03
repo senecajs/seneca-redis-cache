dbb83606512b72682b7a9c151d3fc3b44e9c212a
Seamus D'Arcy <darsee@gmail.com>
2014-04-04 00:09:59 +0100

    Use uuids to avoid having to flush keys between tests

--- test/cache.test.js
+++ test/cache.test.js
@@ -6,21 +6,24 @@ var seneca = require('seneca')();
 seneca.use('..');
 
 var assert = require('assert');
+var uuid = require('uuid');
 
 describe('cache', function() {
 
   var cache = seneca.pin({role: 'cache', cmd: '*'});
+  var a = uuid.v4();
+  var b = uuid.v4();
 
   it('set', function(cb) {
-    cache.set({key: 'a', val: 'one'}, function(err, out) {
+    cache.set({key: a, val: 'one'}, function(err, out) {
       assert.equal(err, null);
-      assert(out, 'a');
+      assert(out, a);
       cb();
     });
   });
 
   it('get', function(cb) {
-    cache.get({key: 'a'}, function(err, out) {
+    cache.get({key: a}, function(err, out) {
       assert.equal(err, null);
       assert.equal(out, 'one');
       cb();
@@ -28,17 +31,17 @@ describe('cache', function() {
   });
 
   it('add', function(cb) {
-    cache.add({key: 'b', val: 1}, function(err, out) {
+    cache.add({key: b, val: 1}, function(err, out) {
       assert.equal(err, null);
-      assert.equal(out, 'b');
+      assert.equal(out, b);
       cb();
     });
   });
 
   it('won\'t add exsting key', function(cb) {
-    cache.add({key: 'b', val: 'something'}, function(err, out) {
+    cache.add({key: b, val: 'something'}, function(err, out) {
       assert(err);
-      cache.get({key: 'b'}, function(err, out) {
+      cache.get({key: b}, function(err, out) {
         assert.equal(out, 1);
         cb();
       });
@@ -46,7 +49,7 @@ describe('cache', function() {
   });
 
   it('incr', function(cb) {
-    cache.incr({key: 'b', val: 4}, function(err, out) {
+    cache.incr({key: b, val: 4}, function(err, out) {
       assert.equal(err, null);
       assert.equal(out, 5);
       cb();
@@ -54,7 +57,7 @@ describe('cache', function() {
   });
 
   it('decr', function(cb) {
-    cache.decr({key: 'b', val: 3}, function(err, out) {
+    cache.decr({key: b, val: 3}, function(err, out) {
       assert.equal(err, null);
       assert.equal(out, 2);
       cb();
@@ -62,24 +65,24 @@ describe('cache', function() {
   });
 
   it('won\'t incr unless value is an integer', function(cb) {
-    cache.incr({key: 'a', val: 1}, function(err, out) {
+    cache.incr({key: a, val: 1}, function(err, out) {
       assert(err);
       cb();
     });
   });
 
   it('won\'t decr if value is not an integer', function(cb) {
-    cache.decr({key: 'a', val: 1}, function(err, out) {
+    cache.decr({key: a, val: 1}, function(err, out) {
       assert(err);
       cb();
     });
   });
 
   it('delete', function(cb) {
-    cache.delete({key: 'a'}, function(err, out) {
+    cache.delete({key: a}, function(err, out) {
       assert.equal(err, null);
-      assert(out, 'a');
-      cache.get({key: 'a'}, function(err, out) {
+      assert(out, a);
+      cache.get({key: a}, function(err, out) {
         assert.equal(err, null);
         assert.equal(out, undefined);
         cb();
